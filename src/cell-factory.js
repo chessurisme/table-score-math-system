c659d58238874d1a8148682ff4591b56930e605b
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-11-24 07:11:06 +0800

    feat: add CellFactory methods for creating player name, score, total, and rank cells

---
+++ src/cell-factory.js
@@ -0,0 +1,44 @@
+const CellFactory = {
+	createContainer() {
+		return document.createElement('div')
+	},
+
+	createPlayerNameCell(playerId) {
+		const container = this.createContainer()
+		container.className = 'name-cell'
+		container.id = `player-${playerId}-name`
+		container.contentEditable = true
+		return container
+	},
+
+	createScoreCell(score, index, playerId) {
+		const container = this.createContainer()
+		container.className = `score-cell score-${score}-cell`
+		container.id = `player-${playerId}-score-${score}-cell-${index}`
+		container.dataset.player = playerId
+		container.dataset.storePoints = score
+		container.dataset.points = 0
+		container.addEventListener('dblclick', () =>
+			ScoreManager.handleDoubleClick.call(ScoreManager, container)
+		)
+		return container
+	},
+
+	createTotalCell(playerId) {
+		const container = this.createContainer()
+		container.className = 'total-cell'
+		container.id = `total-${playerId}`
+		container.textContent = ScoreUtils.manageTotal(playerId) || 0
+		return container
+	},
+
+	createRankCell(playerId) {
+		const container = this.createContainer()
+		container.className = 'rank-cell'
+		container.id = `rank-${playerId}`
+		container.textContent = '-'
+		return container
+	}
+}
+
+export { CellFactory }
