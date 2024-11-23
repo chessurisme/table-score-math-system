17e3102653a34bf9e5502446307ae0ee8cffa24c
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-11-24 07:03:15 +0800

    feat: implement GameState class with player count management

---
+++ src/game-state.js
@@ -0,0 +1,15 @@
+class GameState {
+	#playerCount
+
+	constructor() {
+		this.#playerCount = 1
+	}
+
+	get nextPlayerId() {
+		return this.#playerCount++
+	}
+
+	get currentPlayerCount() {
+		return this.#playerCount
+	}
+}
