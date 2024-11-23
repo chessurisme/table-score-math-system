a5bb3987e4c52d52cda8032e8a4f03e275e2b98f
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-11-24 07:02:26 +0800

    feat: define game configuration with scores, cells, colors, and medals

---
+++ src/game-config.js
@@ -0,0 +1,43 @@
+const GameConfig = {
+	SCORES: {
+		2: 10,
+		3: 5,
+		5: 5
+	},
+
+	CELLS: 22,
+
+	COLORS: {
+		2: {
+			light: '#f8f8ff',
+			dark: '#595983'
+		},
+		3: {
+			light: '#fff8f8',
+			dark: '#e55555'
+		},
+		5: {
+			light: '#f8fff8',
+			dark: '#75b875'
+		}
+	},
+
+	MEDALS: {
+		1: {
+			color: '#FFD700',
+			textColor: 'black'
+		},
+		2: {
+			color: '#C0C0C0',
+			textColor: 'black'
+		},
+		3: {
+			color: '#CD7F32',
+			textColor: 'black'
+		},
+		default: {
+			color: 'white',
+			textColor: 'black'
+		}
+	}
+}
